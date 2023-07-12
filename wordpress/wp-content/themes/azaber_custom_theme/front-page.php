b71caa0adb415ed7e00a9d7d0d0f91aef8d3b069
realazaber <azcooldude8@gmail.com>
2023-07-12 15:51:55 +0200

    Main menu

--- wordpress/wp-content/themes/azaber_custom_theme/front-page.php
+++ wordpress/wp-content/themes/azaber_custom_theme/front-page.php
@@ -12,9 +12,12 @@
 </head>
 
 <body>
+    <?php
+    get_header();
+    ?>
     <div class="container-xxl bg-white p-0">
 
-        <?php get_header(); ?>
+
 
         <?php
         get_template_part('template-parts/full-screen-search');
@@ -32,18 +35,16 @@
             ?>
         </div>
 
-        <?php
-        get_template_part('footer');
-        ?>
-
-        <?php
-        wp_footer();
-        ?>
-
         <!-- Back to Top -->
         <?php
         get_template_part('template-parts/back-to-top');
         ?>
+
+        <?php
+        get_footer("footer.php");
+        wp_footer();
+        ?>
+
     </div>
 </body>
 
